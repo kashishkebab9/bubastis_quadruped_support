6d725a977f4c77a8395f916b91bcacbb5d055076
Kashish Garg <kashishgarg9@gmail.com>
2020-06-08 22:27:23 -0500

    Add files via upload

---
+++ math_stuff/jacobian_rotation_math.py
@@ -0,0 +1,4 @@
+#Script to figure out the joints required to to reach certain base_link roll/pitch/yaw angles
+#Need to convert roll/pitch/yaw angles into different translations
+#Need to determine change in translation required to achieve
+#Use Jacobian Manipulator convert delta translation into delta joint angles
