bf73bb7c9fd14c22156c2a5b43254ff2ae3284e8
Kashish Garg <kashishgarg9@gmail.com>
2020-09-16 20:50:58 -0500

    Add files via upload

--- src/xbox_control/src/jacobian_directional_walking_controller.py
+++ src/xbox_control/src/jacobian_directional_walking_controller.py
@@ -7,6 +7,7 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import Joy
 import math
 import numpy as np
+import bubastis_back_plane as backplane
 
 a_global = 0
 b_global = 0
@@ -19,99 +20,13 @@ l_3 = .5
 q_1_nom =  0
 q_2_nom =  math.pi/4
 q_3_nom = -math.pi/2
+
 #In order to run this script, joy_node needs to be running as well as new_rviz.launch
-class quad_leg:
 
-    def __init__(self, a_offset, b_offset, q_1, q_2, q_3, l_1):
-        #constructor to define certain leg variables
-        self.a_offset = a_offset
-        self.b_offset = b_offset
-        self.q_1 = q_1
-        self.q_2 = q_2
-        self.q_3 = q_3
-        self.l_1 = l_1
-
-
-    def set_joint_angle(self, theta_1, theta_2, theta_3):
-        #set shoulder, elbow, knee angles in radians
-        self.q_1 = theta_1
-        self.q_2 = theta_2
-        self.q_3 = theta_3
-        print(self.q_1)
-        print(self.q_2)
-        print(self.q_3)
-
-    def set_nom_position(self):
-        #set angles to q_1_nom, q_2_nom, q_3_nom
-        #need to be revisited didnt work fully during the directional walking
-        self.q_1 = q_1_nom
-        self.q_2 = q_2_nom
-        self.q_2 = q_2_nom
-
-    def ee_position(self):
-        #algorithm to determine the current x, y, z position of the foot
-
-        global l_2
-        global l_3
-
-        #origin_base_rot = np.matrix([[0, 0, 1, 0], [-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 0, 1]])
-        # base_link to joint_0 transforms
-        base_zero_trans = np.matrix([[1, 0, 0, self.a_offset],[0, 1, 0, self.b_offset],[0, 0, 1, 0], [0, 0, 0, 1]])
-        #joint_0 to joint_1 transforms
-        #l_1 translation will be -l_1 if it is the right side of the vehicle and l_1 if on the left
-        zero_one_rot_z = np.matrix([[math.cos(self.q_1), -1*math.sin(self.q_1), 0, 0], [math.sin(self.q_1), math.cos(self.q_1), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        zero_one_trans = np.matrix([[1, 0, 0, self.l_1], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        zero_one_rot_x = np.matrix([[0, 0, 1, 0], [0, 1, 0, 0], [-1, 0, 0, 0], [0, 0, 0, 1]])
-        #joint_1 to joint_2 transforms
-        one_two_rot_z = np.matrix([[math.cos(self.q_2), -1*math.sin(self.q_2), 0, 0], [math.sin(self.q_2), math.cos(self.q_2), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        one_two_trans = np.matrix([[1, 0, 0, 0], [0, 1, 0, l_2], [0, 0, 1, 0], [0, 0, 0, 1]])
-        #joint_2 to end effector transforms
-        two_ee_rot_z = np.matrix([[math.cos(self.q_3), -1*math.sin(self.q_3), 0, 0], [math.sin(self.q_3), math.cos(self.q_3), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        two_ee_trans = np.matrix([[1, 0, 0, 0], [0, 1, 0, l_3], [0, 0, 1, 0], [0, 0, 0, 1]])
-        base_zero = base_zero_trans
-        zero_one = zero_one_rot_z * zero_one_trans * zero_one_rot_x
-        one_two = one_two_rot_z * one_two_trans
-        two_ee = two_ee_rot_z * two_ee_trans
-        #origin_ee =  base_zero * zero_one * one_two * two_ee
-        base_ee = base_zero * zero_one * one_two * two_ee
-        #print(origin_ee)
-        position_matrix = np.array([[base_ee.item(0, 3)], [base_ee.item(1, 3)], [base_ee.item(2, 3)]])
-        return position_matrix
-
-
-    def get_delta_q(self, z, x, y):
-        
-        delta_x = np.matrix([[-x],[-y],[z]])
-        dx_dq_1 = l_3*math.cos(self.q_1)*math.sin(self.q_2)*math.sin(self.q_3) - l_2*math.cos(self.q_1)*math.cos(self.q_2) - self.l_1*math.sin(self.q_1)
-        dx_dq_2 = l_3*(math.cos(self.q_2)*math.sin(self.q_1)*math.sin(self.q_3) + math.cos(self.q_3)*math.sin(self.q_1)*math.sin(self.q_2)) + l_2*math.sin(self.q_1)*math.sin(self.q_2)
-        dx_dq_3 = l_3*(math.cos(self.q_2)*math.sin(self.q_1)*math.sin(self.q_3) + math.cos(self.q_3)*math.sin(self.q_1)*math.sin(self.q_2))
-        dy_dq_1 = l_3*(math.sin(self.q_1)*math.sin(self.q_2)*math.sin(self.q_3) - math.cos(self.q_2)*math.cos(self.q_3)*math.sin(self.q_1)) + self.l_1*math.cos(self.q_1) - l_2*math.cos(self.q_2)*math.sin(self.q_1)
-        dy_dq_2 = -1*l_3*(math.cos(self.q_1)*math.cos(self.q_2)*math.sin(self.q_3) + math.cos(self.q_1)*math.cos(self.q_3)*math.sin(self.q_2)) - l_2*math.cos(self.q_1)*math.sin(self.q_2)
-        dy_dq_3 = -1*l_3*(math.cos(self.q_1)*math.cos(self.q_2)*math.sin(self.q_3) + math.cos(self.q_1)*math.cos(self.q_3)*math.sin(self.q_2))
-        dz_dq_1 = 0
-        dz_dq_2 = l_3*(math.cos(self.q_2)*math.cos(self.q_3) - math.sin(self.q_2)*math.sin(self.q_3)) + l_2*math.cos(self.q_2)
-        dz_dq_3 = l_3*(math.cos(self.q_2)*math.cos(self.q_3) - math.sin(self.q_2)*math.sin(self.q_3))
-
-
-        jacobian = np.matrix([[dx_dq_1, dx_dq_2, dx_dq_3], [dy_dq_1, dy_dq_2, dy_dq_3], [dz_dq_1, dz_dq_2, dz_dq_3]])
-        #print(jacobian)
-        jacobian_det = ((dx_dq_1)*((dy_dq_2 * dz_dq_3)-(dy_dq_3*dz_dq_2))) - ((dx_dq_2)*((dy_dq_1*dz_dq_3)-(dy_dq_3*dz_dq_1))) + ((dx_dq_3)*((dy_dq_1*dz_dq_2)-(dy_dq_2*dz_dq_1)))
-        #print(jacobian_det)
-        jacobian_inv = np.linalg.inv(jacobian)
-        delta_q = jacobian_inv * delta_x
-
-        q_1_delta = delta_q[0][0]
-        q_2_delta = delta_q[1][0]
-        q_3_delta = delta_q[2][0]
-        self.q_1 = self.q_1 + q_1_delta
-        self.q_2 = self.q_2 + q_2_delta
-        self.q_3 = self.q_3 + q_3_delta
-
-
-rf = quad_leg(-a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, l_1_global)
-lf = quad_leg(a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, -l_1_global)
-rb = quad_leg(-a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, l_1_global)
-lb = quad_leg(a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, -l_1_global)
+rf = backplane.quad_leg(-a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, l_1_global)
+lf = backplane.quad_leg(a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, -l_1_global)
+rb = backplane.quad_leg(-a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, l_1_global)
+lb = backplane.quad_leg(a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, -l_1_global)
 
 hello_str = JointState()
 hello_str.header = Header()
@@ -164,8 +79,6 @@ def callback(data):
                     rf.get_delta_q(-delta_land_parabola_x[i], -delta_land_parabola_y[i], delta_parabola_z[i])
                     rospy.sleep(.1)
                     hello_str.position = [lf.q_1, lf.q_2, lf.q_3, 0, 0, rf.q_1, rf.q_2, rf.q_3, 0, 0, lb.q_1, lb.q_2, lb.q_3, 0, 0, rb.q_1, rb.q_2, rb.q_3, 0, 0]
-                    print "delta_land_parabola_x", delta_land_parabola_x
-                    print "delta_parabola_z", delta_parabola_z
 
             elif f == 1:            
                 for i in range(4):
--- src/xbox_control/src/jacobian_oop_implementation.py
+++ src/xbox_control/src/jacobian_oop_implementation.py
@@ -7,112 +7,24 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import Joy
 import math
 import numpy as np
+import bubastis_back_plane as backplane
 
 a_global = 0
 b_global = 0
 #l_1 is the shoulder
-l_1 = 3.5
+l_1_global = .35
 #l_2 is the elbow
-l_2 = 5.0
+l_2 = .5
 #l_3 is the knee
-l_3 = 5.0
+l_3 = .5
 q_1_nom =  0
 q_2_nom =  math.pi/4
 q_3_nom = -math.pi/2
-#In order to run this script, joy_node needs to be running as well as new_rviz.launch
 
-class quad_leg:
-
-    def __init__(self, a_offset, b_offset, q_1, q_2, q_3):
-        #constructor to define certain leg variables
-        self.a_offset = a_offset
-        self.b_offset = b_offset
-        self.q_1 = q_1
-        self.q_2 = q_2
-        self.q_3 = q_3
-
-    def set_joint_angle(self, theta_1, theta_2, theta_3):
-        #set shoulder, elbow, knee angles in radians
-        self.q_1 = theta_1
-        self.q_2 = theta_2
-        self.q_3 = theta_3
-        print(self.q_1)
-        print(self.q_2)
-        print(self.q_3)
-
-    def set_nom_position(self):
-        #set angles to q_1_nom, q_2_nom, q_3_nom
-        self.q_1 = q_1_nom
-        self.q_2 = q_2_nom
-        self.q_2 = q_2_nom
-
-    def ee_position(self):
-        #algorithm to determine the current x, y, z position of the foot
-        global l_1
-        global l_2
-        global l_3
-
-        #origin_base_rot = np.matrix([[0, 0, 1, 0], [-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 0, 1]])
-        # base_link to joint_0 transforms
-        base_zero_trans = np.matrix([[1, 0, 0, self.a_offset],[0, 1, 0, self.b_offset],[0, 0, 1, 0], [0, 0, 0, 1]])
-        #joint_0 to joint_1 transforms
-        #l_1 translation will be -l_1 if it is the right side of the vehicle and l_1 if on the left
-        zero_one_rot_z = np.matrix([[math.cos(self.q_1), -1*math.sin(self.q_1), 0, 0], [math.sin(self.q_1), math.cos(self.q_1), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        zero_one_trans = np.matrix([[1, 0, 0, l_1], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        zero_one_rot_x = np.matrix([[0, 0, 1, 0], [0, 1, 0, 0], [-1, 0, 0, 0], [0, 0, 0, 1]])
-        #joint_1 to joint_2 transforms
-        one_two_rot_z = np.matrix([[math.cos(self.q_2), -1*math.sin(self.q_2), 0, 0], [math.sin(self.q_2), math.cos(self.q_2), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        one_two_trans = np.matrix([[1, 0, 0, 0], [0, 1, 0, l_2], [0, 0, 1, 0], [0, 0, 0, 1]])
-        #joint_2 to end effector transforms
-        two_ee_rot_z = np.matrix([[math.cos(self.q_3), -1*math.sin(self.q_3), 0, 0], [math.sin(self.q_3), math.cos(self.q_3), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        two_ee_trans = np.matrix([[1, 0, 0, 0], [0, 1, 0, l_3], [0, 0, 1, 0], [0, 0, 0, 1]])
-        base_zero = base_zero_trans
-        zero_one = zero_one_rot_z * zero_one_trans * zero_one_rot_x
-        one_two = one_two_rot_z * one_two_trans
-        two_ee = two_ee_rot_z * two_ee_trans
-        origin_ee = origin_base_rot* base_zero * zero_one * one_two * two_ee
-        base_ee = base_zero * zero_one * one_two * two_ee
-        #print(origin_ee)
-        position_matrix = np.matrix([[origin_ee.item(0, 3)], [origin_ee.item(1, 3)], [origin_ee.item(2, 3)]])
-        print(position_matrix)
-
-    def get_delta_q(self, z, x, y):
-        #OK THIS IS SOME JANKY SHIT, BUUUUUT YOU HAVE TO INPUT (xyz) & FOR REASONS UNKNOWN, THE INDEXING GETS FUCKY
-        #X RESULTS IN CHANGING Y
-        #Y RESULTS IN CHANGING Z
-        #Z RESULTS IN CHANGING
-        #NOT JUST THAT, BUT SOME BECOME NEGATIVE?????? HELLLO?
-        delta_x = np.matrix([[-x],[-y],[z]])
-        dx_dq_1 = l_3*math.cos(self.q_1)*math.sin(self.q_2)*math.sin(self.q_3) - l_2*math.cos(self.q_1)*math.cos(self.q_2) - l_1*math.sin(self.q_1)
-        dx_dq_2 = l_3*(math.cos(self.q_2)*math.sin(self.q_1)*math.sin(self.q_3) + math.cos(self.q_3)*math.sin(self.q_1)*math.sin(self.q_2)) + l_2*math.sin(self.q_1)*math.sin(self.q_2)
-        dx_dq_3 = l_3*(math.cos(self.q_2)*math.sin(self.q_1)*math.sin(self.q_3) + math.cos(self.q_3)*math.sin(self.q_1)*math.sin(self.q_2))
-        dy_dq_1 = l_3*(math.sin(self.q_1)*math.sin(self.q_2)*math.sin(self.q_3) - math.cos(self.q_2)*math.cos(self.q_3)*math.sin(self.q_1)) + l_1*math.cos(self.q_1) - l_2*math.cos(self.q_2)*math.sin(self.q_1)
-        dy_dq_2 = -1*l_3*(math.cos(self.q_1)*math.cos(self.q_2)*math.sin(self.q_3) + math.cos(self.q_1)*math.cos(self.q_3)*math.sin(self.q_2)) - l_2*math.cos(self.q_1)*math.sin(self.q_2)
-        dy_dq_3 = -1*l_3*(math.cos(self.q_1)*math.cos(self.q_2)*math.sin(self.q_3) + math.cos(self.q_1)*math.cos(self.q_3)*math.sin(self.q_2))
-        dz_dq_1 = 0
-        dz_dq_2 = l_3*(math.cos(self.q_2)*math.cos(self.q_3) - math.sin(self.q_2)*math.sin(self.q_3)) + l_2*math.cos(self.q_2)
-        dz_dq_3 = l_3*(math.cos(self.q_2)*math.cos(self.q_3) - math.sin(self.q_2)*math.sin(self.q_3))
-        jacobian = np.matrix([[dx_dq_1, dx_dq_2, dx_dq_3], [dy_dq_1, dy_dq_2, dy_dq_3], [dz_dq_1, dz_dq_2, dz_dq_3]])
-        #print(jacobian)
-        jacobian_det = ((dx_dq_1)*((dy_dq_2 * dz_dq_3)-(dy_dq_3*dz_dq_2))) - ((dx_dq_2)*((dy_dq_1*dz_dq_3)-(dy_dq_3*dz_dq_1))) + ((dx_dq_3)*((dy_dq_1*dz_dq_2)-(dy_dq_2*dz_dq_1)))
-        #print(jacobian_det)
-        jacobian_inv = np.linalg.inv(jacobian)
-        delta_q = jacobian_inv * delta_x
-        print(delta_q)
-        q_1_delta = delta_q[0][0]
-        q_2_delta = delta_q[1][0]
-        q_3_delta = delta_q[2][0]
-        self.q_1 = self.q_1 + q_1_delta
-        self.q_2 = self.q_2 + q_2_delta
-        self.q_3 = self.q_3 + q_3_delta
-        print(self.q_1)
-        print(self.q_2)
-        print(self.q_3)
-
-rf = quad_leg(a_global, -b_global, q_1_nom, q_2_nom, q_3_nom)
-lf = quad_leg(a_global, b_global, q_1_nom, q_2_nom, q_3_nom)
-rb = quad_leg(-a_global, -b_global, q_1_nom, q_2_nom, q_3_nom)
-lb = quad_leg(-a_global, b_global, q_1_nom, q_2_nom, q_3_nom)
+rf = backplane.quad_leg(-a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, l_1_global)
+lf = backplane.quad_leg(a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, -l_1_global)
+rb = backplane.quad_leg(-a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, l_1_global)
+lb = backplane.quad_leg(a_global, -b_global, q_1_nom, q_2_nom, q_3_nom, -l_1_global)
 
 hello_str = JointState()
 hello_str.header = Header()
@@ -131,11 +43,23 @@ def callback(data):
     #print(joy_r_vert)
     joy_r_hor = data.axes[3]
     #print(joy_r_hor)
-    rf.ee_position()
 
+    x_des = joy_r_vert/100
+    y_des = joy_r_hor/100
+    z_des = joy_l_vert/100
+    rf.get_delta_q(x_des, y_des, z_des)
+    lf.get_delta_q(x_des, y_des, z_des)
+    rb.get_delta_q(x_des, y_des, z_des)
+    lb.get_delta_q(x_des, y_des, z_des)
 
-    hello_str.position = [0, 0, 0, 0, 0, q_1, q_2, q_3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    print "rf_position:", rf.ee_position()
+    print "lf_position:", lf.ee_position()
+    print "rb_position:", rb.ee_position()
+    print "lb_position:", lb.ee_position()
 
+    hello_str.position = [lf.q_1, lf.q_2, lf.q_3, 0, 0, rf.q_1, rf.q_2, rf.q_3, 0, 0, lb.q_1, lb.q_2, lb.q_3, 0, 0, rb.q_1, rb.q_2, rb.q_3, 0, 0]
+    time_now = rospy.Time.now()
+    print(time_now)
 
 def talker():
 
@@ -152,6 +76,7 @@ def talker():
       hello_str.header.stamp = rospy.Time.now()
       pub.publish(hello_str)
       rate.sleep()
+
 
 if __name__ == '__main__':
     try:
